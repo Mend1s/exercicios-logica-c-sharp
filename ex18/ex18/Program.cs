299679902cf16ee37fda62002a27c241a4edd084
Mend1s <andremendesskt99@gmail.com>
2021-04-30 14:42:56 -0300

    for

---
+++ ex18/ex18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+//X, se for o caso.
+
+namespace ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
